5a0d5d70480962c22af1438b1b7cad00fe6d71d3
Fadli-Aqil <fadli.aqil12@gmail.com>
2024-03-19 15:03:58 +0700

    release: Vyer Validator

---
+++ README.md
@@ -0,0 +1,42 @@
+Vyer Validator - Simplified String Validation
+
+Vyer Validator offers streamlined string validation, simplifying the process of ensuring data integrity and accuracy in applications.
+
+Installation
+
+You can install the Vyer Validator library using npm:
+
+```bash
+npm i --save vyer-validator
+```
+
+Usage
+
+```javascript
+const string = "";
+const validator = VyerValidator(string).IsRequired().MinLength(5);
+validator.Validate(); // ==> false, because the string is empty and less than 5 characters
+validator.Errors(); // => returns an array of error messages
+```
+
+API Documentation
+
+Response
+Vyer Validator returns a response that provides the following methods:
+
+- `Validate()`: Validates your validators and returns a boolean value.
+- `Errors()`: Checks for rule exceptions and returns error messages.
+
+Built-in Validators
+
+Vyer Validator provides the following built-in validators:
+
+- `IsRequired(errorMessage)`: Validates if the string is a non-empty value.
+- `EmailOnly(errorMessage)`: Validates if the string is in Email Format.
+- `MaxLength(max, errorMessage)`: Validates the maximum length of the value.
+- `MinLength(min, errorMessage)`: Validates the minimum length of the value.
+- `RangeNumeric(min, max, errorMessage)`: Validates the range of numeric values.
+
+License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ src/validation.ts
@@ -0,0 +1,68 @@
+class Validation {
+  field: string | null | undefined;
+  errorMessage: string[];
+
+  constructor(field: string | null | undefined) {
+    this.field = field;
+    this.errorMessage = [];
+  }
+
+  IsRequired(errorMessage?: string) {
+    if (this.field == null || this.field === "") {
+      errorMessage ? this.errorMessage.push(errorMessage) : this.errorMessage.push("This field is Required");
+    }
+    return this;
+  }
+
+  MaxLength(max: number, errorMessage?: string) {
+    if (this.field?.length! > max) {
+      errorMessage ? this.errorMessage.push(errorMessage) : this.errorMessage.push(`This field max ${max} character`);
+    }
+    return this;
+  }
+
+  MinLength(min: number, errorMessage?: string) {
+    if (this.field?.length! < min) {
+      errorMessage ? this.errorMessage.push(errorMessage) : this.errorMessage.push(`This field min ${min} character`);
+    }
+    return this;
+  }
+
+  EmailOnly(errorMessage?: string) {
+    const pattern: RegExp = new RegExp(
+      "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \\t]|(\\[\\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\\t -Z^-~]*])"
+    );
+
+    if (!pattern.test(this.field!)) {
+      errorMessage ? this.errorMessage.push(errorMessage) : this.errorMessage.push("This field must Email format");
+    }
+
+    return this;
+  }
+
+  RangeNumeric(min: number, max: number, errorMessage?: string) {
+    const numeric = parseFloat(this.field!);
+
+    if (isNaN(numeric)) {
+      this.errorMessage.push("This field must be numeric");
+    }
+
+    if (numeric < min || numeric > max) {
+      errorMessage ? this.errorMessage.push(errorMessage) : this.errorMessage.push(`This field have min ${min} and max ${max}`);
+    }
+
+    return this;
+  }
+
+  Validate() {
+    return this.errorMessage.length > 0 ? false : true;
+  }
+
+  Errors() {
+    return this.errorMessage;
+  }
+}
+
+export function VyerValidation(field: string | null | undefined) {
+  return new Validation(field);
+}
